4f832e2987fc9a274a4499d29df0f5e5df782e56
NikaShabashvili03 <shabashvilinika@yahoo.com>
2025-03-31 06:00:33 +0400

    update 2.0

--- src/components/SwipeItem.tsx
+++ src/components/SwipeItem.tsx
@@ -1,144 +1,85 @@
-import { animate, motion, useMotionValue, useTransform } from "framer-motion";
-import 'swiper/css';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination } from 'swiper/modules'
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import NopeIcon from '../assets/icons/nope-icon.png'
-import LikeIcon from '../assets/icons/like-icon.png'
-import { useCallback, useEffect, useState } from "react";
-
-import Nope from '../assets/icons/nope.png'
-import Love from '../assets/icons/love.png'
+import { motion, useMotionValue, useTransform } from "framer-motion";
+import { useEffect, useState } from "react";
+import NopeIcon from '../assets/icons/nope-icon.png';
+import LikeIcon from '../assets/icons/like-icon.png';
 import { UserType } from "../types";
 
-
-
 interface SwipeItemProps extends UserType {
-  index: number,
-  setUsers: React.Dispatch<React.SetStateAction<UserType[]>>;
+  index: number;
+  onNope: (index: number) => void;
+  onLike: (index: number) => void;
+  trackX: ({ x, isDragged }: { x: number, isDragged: boolean }) => void;
 }
 
-export default function SwipeItem({
-  images,
-  name,
-  age,
-  index,
-  setUsers
-}: SwipeItemProps) {
-
+export default function SwipeItem({ images, name, age, index, trackX, onNope, onLike }: SwipeItemProps) {
   const x = useMotionValue(0);
-  const xInput = [-100, 0, 100]
-  const opacityInput = [-100, 0, 100]
-  const xRotate = useTransform(x, xInput, [-10, 0, 10])
-  const opacity = useTransform(x, opacityInput, [0, 0, 1])
-  const opacity2 = useTransform(x, opacityInput, [1, 0, 0])
-  const [IsDragged, setIsDragged] = useState(false);
-  
-  const onLike = useCallback(() => {
-    setUsers((prev) => prev.filter((_, i) => i !== index));
-  }, [setUsers, index]);
-  
-  const onNope = useCallback(() => {
-    setUsers((prev) => prev.filter((_, i) => i !== index));
-  }, [setUsers, index]);
+  const xInput = [-100, 0, 100];
+  const xRotate = useTransform(x, xInput, [-10, 0, 10]);
+  const opacity = useTransform(x, xInput, [0, 0, 1]);
+  const opacity2 = useTransform(x, xInput, [1, 0, 0]);
+
+  const [isDragged, setIsDragged] = useState(false);
 
   useEffect(() => {
     const unsubscribe = x.on("change", (latest) => {
-      if(!IsDragged){
+      const trackData = {
+        x: latest,
+        isDragged: isDragged
+      };
+      
+      trackX(trackData);
+  
+      if (!isDragged) {
         if (latest < -100) {
-            onNope()
+          onNope(index);
         } else if (latest > 100) {
-            onLike();
+          onLike(index);
         }
       }
     });
   
     return () => unsubscribe();
-  }, [x, IsDragged, onLike, onNope]);
-  
+  }, [x, isDragged, onNope, onLike, index, trackX]);
+
   return (
-    <div style={{ zIndex: index }} className="h-dvh flex justify-center w-full absolute items-center">
-      <motion.div className="flex justify-center bg-black/50 items-center w-full overflow-hidden h-full">
-        <motion.div
-          style={{ 
-            x: x,
-            rotate: xRotate,
-          }} 
-          drag="x"
-          dragConstraints={{ left: 0, right: 0 }} 
-          dragElastic={0.5}  
-          dragTransition={{ bounceStiffness: 300, bounceDamping: 25 }}
-          whileDrag={{ cursor: "grabbing" }}
-          onDragStart={() => setIsDragged(true)}
-          onDragEnd={() => setIsDragged(false)}
-        >
-          <div className="w-[400px] relative h-[600px]">
-              <Swiper
-                  spaceBetween={10}
-                  slidesPerView={1}
-                  className="h-full"
-                  loop={true}
-                  modules={[ Navigation, Pagination ]}
-                  navigation={true}
-                  allowTouchMove={false}
-                  pagination={{
-                    type: "custom",
-                    renderCustom: function (_, current, total) {
+    <div style={{ zIndex: index }} className="h-fit absolute flex justify-center w-fit items-center">
+      <motion.div
+        style={{ x, rotate: xRotate }}
+        drag
+        dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
+        dragElastic={1}
+        dragTransition={{ bounceStiffness: 300, bounceDamping: 25 }}
+        whileDrag={{ cursor: "grabbing" }}
+        transition={{ type: "spring", stiffness: 150, damping: 20 }}
+        onDragStart={() => setIsDragged(true)}
+        onDragEnd={() => setIsDragged(false)}
+      >
+        <div className="w-[400px] relative h-[600px]">
+          <div
+            style={{
+              backgroundImage: `url(${images[0]})`,
+              backgroundSize: 'cover',
+              backgroundRepeat: 'no-repeat',
+              backgroundColor: "black",
+              backgroundPosition: "center"
+            }}
+            className="h-full flex justify-end items-end relative w-full"
+          ></div>
 
-                      const paginationDots = new Array(total).fill(null).map((_, index) => {
-                        return `
-                          <span class="w-full  h-[5px] rounded-full border border-white ${index === current - 1 ? 'bg-white' : 'bg-transparent'}">
-                                
-                          </span>
-                        `;
-                      });
-                  
+          <motion.img src={NopeIcon} alt="nope" style={{ opacity: opacity2 }} className="absolute z-50 -rotate-45 w-[100px] h-[80px] top-5 left-5"></motion.img>
+          <motion.img src={LikeIcon} alt="like" style={{ opacity }} className="absolute z-50 rotate-45 top-5 right-5 w-[100px] h-[80px]"></motion.img>
 
-                      return `
-                        <div class="w-full flex gap-1">
-                          ${paginationDots.join('')}
-                        </div>
-                      `
-                    },
-                  }}
-                >
-                  {images.map((img: string, i: number) => 
-                    <SwiperSlide
-                      style={{
-                        backgroundImage: `url(${img})`,
-                        backgroundSize: 'cover',
-                        backgroundRepeat: 'no-repeat',
-                        backgroundColor: "black",
-                        backgroundPosition: "center"
-                      }}
-                      key={i} className="!h-full flex justify-end items-end !relative !w-full"></SwiperSlide>
-                  )}
-              </Swiper>
-              <motion.img src={NopeIcon} alt="nope" style={{ opacity: opacity2 }} className="absolute z-50 -rotate-45 w-[100px] h-[80px] top-5 left-5 text-white text-2xl font-bold"></motion.img>
-              <motion.img src={LikeIcon} alt="like" style={{ opacity: opacity }} className="absolute z-50 rotate-45 top-5 right-5 w-[100px] h-[80px] text-white text-2xl font-bold"></motion.img>
-              <div className="absolute flex flex-col justify-between px-2 py-2 gap-5 w-full z-50 bottom-0">
-                  <div className="flex flex-col gap-1">
-                    <h2 className="text-2xl font-bold text-white">{name} <span className="font-normal text-xl">{age}</span></h2>
-                    <div className="text-gray-300 flex items-center gap-2">
-                      <button className="px-4 py-1 text-sm border-gray-300 rounded-full border-1">Ceo</button>
-                      <button className="px-4 py-1 text-sm border-gray-300 rounded-full border-1">Ragaca</button>
-                      <button className="px-4 py-1 text-sm border-gray-300 rounded-full border-1">2</button>
-                      <button className="px-4 py-1 text-sm border-gray-300 rounded-full border-1">13</button>
-                    </div>
-                  </div>
-                  <div className="flex justify-evenly items-center">
-                    <button onClick={() => animate(x, -150, { type: "spring", stiffness: 300, damping: 25, onComplete: () => animate(x, 0, { type: "spring", stiffness: 300, damping: 25 }), })} className="w-14 h-14 cursor-pointer rounded-full">
-                        <img src={Nope} className="w-full h-full" alt="nope"/>
-                    </button>
-                    <button onClick={() => animate(x, 150, { type: "spring", stiffness: 300, damping: 25, onComplete: () => animate(x, 0, { type: "spring", stiffness: 300, damping: 25 }), })} className="w-14 h-14 cursor-pointer rounded-full">
-                        <img src={Love} className="w-full h-full" alt="love"/>
-                    </button>
-                  </div>
+          <div className="absolute flex flex-col px-2 py-10 gap-5 w-full z-50 bottom-0">
+            <div className="flex flex-col gap-1">
+              <h2 className="text-2xl font-bold text-white">{name} <span className="font-normal text-xl">{age}</span></h2>
+              <div className="text-gray-300 flex items-center gap-2">
+                <button className="px-4 py-1 text-sm border-gray-300 rounded-full border-1">Ceo</button>
+                <button className="px-4 py-1 text-sm border-gray-300 rounded-full border-1">Ragaca</button>
               </div>
+            </div>
           </div>
-        </motion.div>
+        </div>
       </motion.div>
     </div>
-  )
+  );
 }
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,53 +1,105 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SwipeItem from "../components/SwipeItem";
 import { UserType } from "../types";
+import Nope from '../assets/icons/nope.png';
+import Love from '../assets/icons/love.png';
+import { motion } from "framer-motion";
 
-
-const initialUsers = [
+const initialUsers: UserType[] = [
   {
-    images: [
-      'https://picsum.photos/400/600',
-      'https://picsum.photos/400/601',
-      'https://picsum.photos/400/602'
-    ],
+    images: ['https://picsum.photos/400/600', 'https://picsum.photos/400/601', 'https://picsum.photos/400/602'],
     name: "Nika",
     age: 21
   },
   {
-    images: [
-      'https://picsum.photos/400/603',
-      'https://picsum.photos/400/604',
-      'https://picsum.photos/400/605'
-    ],
+    images: ['https://picsum.photos/400/603', 'https://picsum.photos/400/604', 'https://picsum.photos/400/605'],
     name: "Jake",
     age: 35
   },
   {
-    images: [
-      'https://picsum.photos/400/606',
-      'https://picsum.photos/400/607',
-      'https://picsum.photos/400/608'
-    ],
+    images: ['https://picsum.photos/400/606', 'https://picsum.photos/400/607', 'https://picsum.photos/400/608'],
     name: "Nope",
     age: 50
   }
-]
+];
 
 
 export default function Main() {
-  const [users, setUsers] = useState<UserType[]>(initialUsers)
+  const [users, setUsers] = useState<UserType[]>(initialUsers);
+  const [currentIndex, setCurrentIndex] = useState(users.length - 1);
+  const [xValue, setXValue] = useState({ x: 0, isDragged: false });
+
+  useEffect(() => {
+    setCurrentIndex(users.length - 1);
+  }, [users]);
+
+  const handleSwipe = (index: number) => {
+    setUsers((prev) => prev.filter((_, i) => i !== index));
+    setXValue({ x: 0, isDragged: false })
+  };
+
+  const handleTrackX = ({ x, isDragged }: { x: number, isDragged: boolean }) => {
+    setXValue({
+      x: x,
+      isDragged: isDragged
+    });
+  };
+
+  const getButtonStyleByX = (isNope: boolean) => {
+    if (isNope && xValue.x < -100) {
+      return "w-20 h-20 !flex"; 
+    } else if (!isNope && xValue.x > 100) {
+      return "w-20 h-20 !flex";
+    }
+    return "w-14 h-14"; 
+  };
+
+  const getButtonStyleByDragged = () => {
+    if ((xValue.x < 0 || xValue.x > 0) && xValue.isDragged){
+      return "hidden"
+    }else {
+      return "flex"
+    }
+  }
+
+
   return (
-    <div className="flex justify-center bg-black/50 items-center relative h-dvh">
-        {users.length <= 0 ? (
-            <div className="text-2xl flex gap-5 flex-col">
-              <h2 className="font-bold text-white">No More Swipes Today...</h2>
-              <button className="text-white cursor-pointer" onClick={() => setUsers(initialUsers)}>Load More..</button>
-            </div>
-        ) : 
-          users.map(({ images, name, age }, index) => (
-            <SwipeItem setUsers={setUsers} key={index} index={index} images={images} name={name} age={age}/>
-          ))
-        }
+    <div className="flex justify-center overflow-hidden bg-black items-center relative h-dvh">
+      {users.length <= 0 ? (
+        <div className="text-2xl flex gap-5 flex-col">
+          <h2 className="font-bold text-white">No More Swipes Today...</h2>
+          <button className="text-white cursor-pointer" onClick={() => setUsers(initialUsers)}>Load More..</button>
+        </div>
+      ) : (
+        <div className="flex flex-col relative justify-center items-center">
+          {users.map(({ images, name, age }, index) => (
+            <SwipeItem
+              key={index}
+              index={index}
+              images={images}
+              name={name}
+              age={age}
+              onLike={handleSwipe}
+              onNope={handleSwipe}
+              trackX={handleTrackX} 
+            />
+          ))}
+          <div className="text-white mt-[600px] w-[400px] absolute z-50 flex justify-evenly items-center">
+            <motion.button
+              onClick={() => handleSwipe(currentIndex)}
+              className={`cursor-pointer rounded-full ${getButtonStyleByX(true)} ${getButtonStyleByDragged()}`}
+            >
+              <img src={Nope} className="w-full h-full" alt="nope" />
+            </motion.button>
+            <motion.button
+              onClick={() => handleSwipe(currentIndex)}
+              className={`cursor-pointer rounded-full ${getButtonStyleByX(false)} ${getButtonStyleByDragged()}`}
+            >
+              <img src={Love} className="w-full h-full" alt="love" />
+            </motion.button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
